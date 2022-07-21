fa5c4f9a5107e7f5eab8898f6da954dd4e528dda
Saemi Jung <jung.sammie@gmail.com>
2022-07-21 23:32:57 +0900

    Create custom mui theme and apply to main layout

--- layouts/MainLayout.js
+++ layouts/MainLayout.js
@@ -1,22 +1,37 @@
 import Nav from "@components/Nav";
 import Footer from "@components/Footer";
-import { Box, Container } from "@mui/material";
+import { Box, Container, CssBaseline } from "@mui/material";
 import { useRouter } from "next/router";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#F9F6ED",
+    },
+    primary: {
+      main: "#09691E",
+    },
+  },
+});
 
 const MainLayout = ({ children }) => {
   const { asPath } = useRouter();
 
   return (
     <>
-      <Container>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
         <Box sx={{ minHeight: "100vh" }}>
           <Nav pathname={asPath} />
-          <Box py={10}>{children}</Box>
+          <Container>
+            <Box py={10}>{children}</Box>
+          </Container>
         </Box>
         <Box>
           <Footer />
         </Box>
-      </Container>
+      </ThemeProvider>
     </>
   );
 };
