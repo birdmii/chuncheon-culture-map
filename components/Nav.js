4257ed43589ae9bdb8f4c702ade4610d0bcde80a
Saemi Jung <jung.sammie@gmail.com>
2022-07-21 23:35:59 +0900

    Apply media queries to the Navbar and Add logo

--- components/Nav.js
+++ components/Nav.js
@@ -1,46 +1,57 @@
 import Link from "next/link";
-import { Grid, Box } from "@mui/material";
+import { Grid, Box, Typography, Container } from "@mui/material";
+import Image from "next/image";
+import Logo from "@assets/Logo.svg";
 
 const Header = ({ pathname }) => {
   return (
     <Grid
       container
       sx={{
-        display: "flex",
-        alignItems: "center",
-        py: 2,
+        borderBottom: "1px solid",
+        borderColor: "primary.main",
       }}
     >
-      <Grid item xs={10}>
-        <Box fontSize={32} fontWeight={300}>
-          <Link href="/">春川文化地圖</Link>
-        </Box>
-      </Grid>
-      <Grid item xs={2}>
-        <Grid
-          container
-          sx={{ display: "flex", justifyContent: "space-between" }}
-        >
-          <Box
-            fontWeight={300}
-            sx={{
-              textDecoration: pathname === "/about" ? "underline" : "none",
-              textUnderlineOffset: "5px",
-            }}
-          >
-            <Link href="/about">소개</Link>
-          </Box>
-          <Box
-            fontWeight={300}
-            sx={{
-              textDecoration: pathname === "/map" ? "underline" : "none",
-              textUnderlineOffset: "5px",
-            }}
-          >
-            <Link href="/map">지도에서 보기</Link>
-          </Box>
+      <Container sx={{ display: "flex", alignItems: "center", py: 2 }}>
+        <Grid item md={10} sm={9} xs={7}>
+          <Link href={"/"} forwardRef>
+            <Box fontSize={20} fontWeight={300} sx={{ display: "flex" }}>
+              <Image src={Logo} />
+            </Box>
+          </Link>
         </Grid>
-      </Grid>
+        <Grid item md={2} sm={3} xs={5}>
+          <Grid
+            container
+            sx={{ display: "flex", justifyContent: "space-between" }}
+          >
+            <Box
+              fontWeight={300}
+              sx={{
+                textDecoration: pathname === "/about" ? "underline" : "none",
+                textUnderlineOffset: "5px",
+                textDecorationColor: `#09691E`,
+              }}
+            >
+              <Link href="/about">
+                <Typography color="primary">소개</Typography>
+              </Link>
+            </Box>
+            <Box
+              fontWeight={300}
+              sx={{
+                textDecoration: pathname === "/map" ? "underline" : "none",
+                textUnderlineOffset: "5px",
+                textDecorationColor: `#09691E`,
+              }}
+            >
+              <Link href="/map">
+                <Typography color="primary">지도에서 보기</Typography>
+              </Link>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
     </Grid>
   );
 };
