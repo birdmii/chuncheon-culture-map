7a6d32975c65156fba05f59453a9fb12ebd024c0
Saemi Jung <jung.sammie@gmail.com>
2022-07-27 15:13:23 +0900

    Remove container in Nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Grid, Box, Typography, Container } from "@mui/material";
+import { Grid, Box, Typography } from "@mui/material";
 import Image from "next/image";
 import Logo from "@assets/logo.svg";
 
@@ -10,48 +10,49 @@ const Header = ({ pathname }) => {
       sx={{
         borderBottom: "1px solid",
         borderColor: "primary.main",
+        p: 2,
+        display: "flex",
+        alignItems: "center",
       }}
     >
-      <Container sx={{ display: "flex", alignItems: "center", py: 2 }}>
-        <Grid item md={10} sm={9} xs={7}>
-          <Link href={"/"} forwardRef>
-            <Box fontSize={20} fontWeight={300} sx={{ display: "flex" }}>
-              <Image src={Logo} />
-            </Box>
-          </Link>
-        </Grid>
-        <Grid item md={2} sm={3} xs={5}>
-          <Grid
-            container
-            sx={{ display: "flex", justifyContent: "space-between" }}
+      <Grid item md={10} sm={9} xs={7}>
+        <Link href={"/"} forwardRef>
+          <Box fontSize={20} fontWeight={300} sx={{ display: "flex" }}>
+            <Image src={Logo} />
+          </Box>
+        </Link>
+      </Grid>
+      <Grid item md={2} sm={3} xs={5}>
+        <Grid
+          container
+          sx={{ display: "flex", justifyContent: "space-around" }}
+        >
+          <Box
+            fontWeight={300}
+            sx={{
+              textDecoration: pathname === "/about" ? "underline" : "none",
+              textUnderlineOffset: "5px",
+              textDecorationColor: `#09691E`,
+            }}
           >
-            <Box
-              fontWeight={300}
-              sx={{
-                textDecoration: pathname === "/about" ? "underline" : "none",
-                textUnderlineOffset: "5px",
-                textDecorationColor: `#09691E`,
-              }}
-            >
-              <Link href="/about">
-                <Typography color="primary">소개</Typography>
-              </Link>
-            </Box>
-            <Box
-              fontWeight={300}
-              sx={{
-                textDecoration: pathname === "/map" ? "underline" : "none",
-                textUnderlineOffset: "5px",
-                textDecorationColor: `#09691E`,
-              }}
-            >
-              <Link href="/map">
-                <Typography color="primary">지도에서 보기</Typography>
-              </Link>
-            </Box>
-          </Grid>
+            <Link href="/about">
+              <Typography color="primary">소개</Typography>
+            </Link>
+          </Box>
+          <Box
+            fontWeight={300}
+            sx={{
+              textDecoration: pathname === "/map" ? "underline" : "none",
+              textUnderlineOffset: "5px",
+              textDecorationColor: `#09691E`,
+            }}
+          >
+            <Link href="/map">
+              <Typography color="primary">지도에서 보기</Typography>
+            </Link>
+          </Box>
         </Grid>
-      </Container>
+      </Grid>
     </Grid>
   );
 };
