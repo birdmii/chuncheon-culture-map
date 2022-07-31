b5b46c326a41b45d25fd2f7153c2369ac440525f
Saemi Jung <jung.sammie@gmail.com>
2022-07-31 22:45:54 +0900

    Install and setup Redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import "@styles/globals.css";
+import { store } from "../store";
+import { Provider } from "react-redux";
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
